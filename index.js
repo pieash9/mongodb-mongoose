1626f9a1b55282372c45cf099890c595c0c42315
Md. pieash ahmed <81944951+pieash9@users.noreply.github.com>
2023-08-08 10:00:52 +0600

    schema and model create

--- index.js
+++ index.js
@@ -4,6 +4,20 @@ const mongoose = require("mongoose");
 const app = express();
 const port = 3000;
 
+//create product schema
+const productsSchema = mongoose.Schema({
+  title: String,
+  price: Number,
+  description: String,
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+// create product model
+const Product = mongoose.model("Products", productsSchema);
+
 const connectDB = async () => {
   try {
     await mongoose.connect("mongodb://127.0.0.1:27017/testProductDB");
