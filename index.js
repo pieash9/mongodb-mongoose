a6268055f703401a4da180a22bb987dc22500f16
Md. pieash ahmed <81944951+pieash9@users.noreply.github.com>
2023-08-11 08:31:56 +0600

    put api

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const express = require("express");
-const mongoose = require("mongoose");
-
 const app = express();
+const mongoose = require("mongoose");
 const port = 3000;
 
 // middleware
@@ -9,24 +8,31 @@ app.use(express.json()); //  for send json data
 app.use(express.urlencoded({ extended: true }));
 
 //create product schema
-const productsSchema = mongoose.Schema({
-  title: {
-    type: String,
-    required: true,
+const productsSchema = mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
+    rating: {
+      type: Number,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+    },
   },
-  price: {
-    type: Number,
-    required: true,
-  },
-  description: {
-    type: String,
-    required: true,
-  },
-  createdAt: {
-    type: Date,
-    default: Date.now,
-  },
-});
+  { versionKey: false }
+);
 
 // create product model
 const Product = mongoose.model("Products", productsSchema);
@@ -46,11 +52,14 @@ const connectDB = async () => {
 app.get("/products", async (req, res) => {
   try {
     const price = req.query.price;
+    const rating = req.query.rating;
     let products;
-    if (price) {
-      products = await Product.find({ price: { $gt: price } });
+    if (price && rating) {
+      products = await Product.find({
+        $or: [{ price: { $gt: price } }, { rating: { $gt: rating } }],
+      }).sort({ price: 1 });
     } else {
-      products = await Product.find();
+      products = await Product.find().sort({ price: -1 });
     }
     if (products.length == 0) {
       res.status(400).send({ message: "product not found" });
@@ -86,6 +95,48 @@ app.post("/products", async (req, res) => {
   } catch (error) {
     res.status(500).send({ message: error.message });
   }
+});
+
+app.put("/products/:id", async (req, res) => {
+  try {
+    const id = req.params.id;
+    const title = req.body.title;
+    const price = req.body.price;
+    const rating = req.body.rating;
+    const description = req.body.description;
+    const updatedProduct = await Product.findByIdAndUpdate(
+      { _id: id },
+      {
+        $set: {
+          title: title,
+          price: price,
+          rating: rating,
+          description: description,
+        },
+      },
+      { new: true }
+    );
+    if (updatedProduct) {
+      res.status(200).send({
+        success: true,
+        message: "product updated",
+        data: updatedProduct,
+      });
+    } else {
+      res.status(404).send({
+        success: false,
+        message: "product was not updated",
+      });
+    }
+  } catch (error) {
+    res.status(500).send({ message: error.message });
+  }
+});
+
+app.delete("/products/:id", async (req, res) => {
+  const id = req.params.id;
+  const result = await Product.findByIdAndDelete({ _id: id });
+  res.send(result);
 });
 
 app.listen(port, async () => {
